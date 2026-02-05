ad78cdd582a7e88f3b8c8997c7d8cbbaa682f4bd
TuF3i <13759301652@163.com>
2026-02-05 17:21:22 +0800

    ✨ 完成gorm的日志适配器

---
+++ apps/public/logger/adapter/gorm-adapter.go
@@ -0,0 +1,72 @@
+package adapter
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+	"gorm.io/gorm/logger"
+)
+
+type LokiGormLogger struct {
+	lokiLogger    *zap.Logger
+	LogLevel      logger.LogLevel
+	SlowThreshold time.Duration
+}
+
+// NewLokiGormLogger 注册新日志核心
+func NewLokiGormLogger(lokiLogger *zap.Logger) *LokiGormLogger {
+	return &LokiGormLogger{
+		lokiLogger:    lokiLogger,
+		LogLevel:      logger.Info,
+		SlowThreshold: 500 * time.Millisecond,
+	}
+}
+
+// LogMode 设置日志级别
+func (r *LokiGormLogger) LogMode(level logger.LogLevel) logger.Interface {
+	newLogger := *r
+	newLogger.LogLevel = level
+	return &newLogger
+}
+
+func (r *LokiGormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
+	if r.LogLevel >= logger.Info {
+		r.lokiLogger.Sugar().Infof(msg, data...)
+	}
+}
+
+func (r *LokiGormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
+	if r.LogLevel >= logger.Info {
+		r.lokiLogger.Sugar().Warnf(msg, data...)
+	}
+}
+
+func (r *LokiGormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
+	if r.LogLevel >= logger.Info {
+		r.lokiLogger.Sugar().Errorf(msg, data...)
+	}
+}
+
+func (r *LokiGormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if r.LogLevel <= logger.Silent {
+		return
+	}
+
+	elapsed := time.Since(begin)
+	sql, rows := fc()
+	fields := []zap.Field{
+		zap.Duration("elapsed", elapsed),
+		zap.Int64("rows", rows),
+		zap.String("sql", sql),
+	}
+
+	switch {
+	case err != nil && r.LogLevel >= logger.Error:
+		r.lokiLogger.Error("gorm trace error", append(fields, zap.Error(err))...)
+	case elapsed > r.SlowThreshold && r.SlowThreshold != 0 && r.LogLevel >= logger.Warn:
+		r.lokiLogger.Warn("gorm slow query", fields...)
+	case r.LogLevel >= logger.Info:
+		r.lokiLogger.Info("gorm query", fields...)
+	}
+}
